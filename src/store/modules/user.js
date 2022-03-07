377549356da721d09cacf27d4c27e992524b8513
xiongxiong <1379061987@qq.com>
2022-03-08 03:37:38 +0800

    封装登录的Action

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -95,9 +95,44 @@
 //   actions
 // }
 
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+
+// 状态
+const state = {
+  // 初始化vuex的时候，就先从缓存中读取
+  token: getToken() // 设置token为共享状态
+}
+
+// 修改状态
+const mutations = {
+  setToken(state, token) {
+    state.token = token// 将数据设置给vuex
+    // 同步给缓存
+    setToken(token)
+  },
+  removeToken(state) {
+    state.token = null// 将vuex的数据置空
+    removeToken()// 同步到缓存
+  }
+}
+
+// 执行异步
+const actions = {
+  async login(context, data) {
+    // 调用api接口
+    const result = await login(data)
+    // axios默认加了一层data
+    if (result.data.success) {
+      // 如果为true，表示登陆成功
+      context.commit('setToken', result.data.data)
+    }
+  }
+}
+
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
