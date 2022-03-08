fd9f8eaeee8938738a9769556c8f7212a64bb7e4
xiongxiong <1379061987@qq.com>
2022-03-08 22:01:23 +0800

    权限拦截路由守卫的编写

--- src/permission.js
+++ src/permission.js
@@ -62,3 +62,44 @@
 //   // finish progress bar
 //   NProgress.done()
 // })
+
+// 权限拦截在路由跳转   导航首位
+
+import router from '@/router'
+import store from '@/store' // 引入store实例   和组件中的this.$store是一回事
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 不需要导出   因为main已经引入了这个文件,只需要让这个代码执行即可
+
+// 前置守卫
+// next是前置首位必须执行的钩子   next必须执行,如果不执行,页面就死了
+// next()放过
+// next(false)跳转终止
+// next(地址)跳转到某个地址
+const whiteList = ['/login', '/404']// 定义白名单
+router.beforeEach((to, from, next) => {
+  nprogress.start()// 开启进度条
+  if (store.getters.token) {
+    // 如果有token
+    if (to.path === '/login') { // 如果要访问的是登录页
+      next('/')// 跳到主页
+    } else {
+      next()
+    }
+  } else {
+    // 没有token的情况下
+    if (whiteList.indexOf(to.path) > -1) {
+      // 表示要去的地址在白名单
+      next()
+    } else {
+      next('/login')
+    }
+  }
+  nprogress.done() // 手动强制关闭一次    为了解决手动切换地址时   进度条不关闭的问题
+})
+
+// 后置守卫
+router.afterEach(() => {
+  nprogress.done()// 关闭进度条
+})
