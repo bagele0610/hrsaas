5b0b35be109fd1b61e70bb6e5ae2b2c56474a719
yuhuofei <1379061987@qq.com>
2022-03-26 18:12:03 +0800

    给当前组织架构添加加载进度条

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="dashboard-container">
+  <div
+    v-loading="loading"
+    class="dashboard-container"
+  >
     <div class="app-container">
       <!-- 组织架构的内容  -头部 -->
       <el-card class="tree-card">
@@ -56,7 +59,8 @@ export default {
         // children: 'children', // 从这个属性去找子节点
       },
       showDialog: false, // 默认不显示弹层
-      node: null// 记录当前点击的node节点
+      node: null,// 记录当前点击的node节点
+      loading: false
 
     }
   },
@@ -65,9 +69,11 @@ export default {
   },
   methods: {
     async getDePartments() {
+      this.loading = true
       const result = await getDePartments()
       this.company = { name: result.companyName, manager: '负责人', id: '' }
       this.departs = tranListToTreeData(result.depts, '') // 需要转换为树形结构
+      this.loading = false
     },
     // 监听tree-tools中触发的点击添加子部门的事件
     // node是点击的部门
