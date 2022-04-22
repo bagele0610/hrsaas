aaa4b1941dbe07d96ebb9d08f66148a6eda633da
yuhuofei <1379061987@qq.com>
2022-04-22 17:54:15 +0800

    公司角色删除功能

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -26,7 +26,8 @@
                 width="120"
                 type="index"
                 align="center"
-              ></el-table-column>
+              >
+              </el-table-column>
               <el-table-column
                 prop="
                 name"
@@ -38,18 +39,22 @@
                 label="描述"
               ></el-table-column>
               <el-table-column label="操作">
-                <el-button
-                  size="small"
-                  type="success"
-                >分配权限</el-button>
-                <el-button
-                  size="small"
-                  type="primary"
-                >编辑</el-button>
-                <el-button
-                  size="small"
-                  type="danger"
-                >删除</el-button>
+                <!-- 作用域插槽 -->
+                <template slot-scope="{row}">
+                  <el-button
+                    size="small"
+                    type="success"
+                  >分配权限</el-button>
+                  <el-button
+                    size="small"
+                    type="primary"
+                  >编辑</el-button>
+                  <el-button
+                    @click="deleteRole(row.id)"
+                    size="small"
+                    type="danger"
+                  >删除</el-button>
+                </template>
               </el-table-column>
             </el-table>
             <!-- 分页组件 -->
@@ -83,24 +88,35 @@
             >
               <el-form-item label="公司名称">
                 <el-input
+                  v-model="formData.name"
                   disabled
                   style="width:400px"
                 />
               </el-form-item>
               <el-form-item label="公司地址">
                 <el-input
+                  v-model="formData.companyAddress"
+                  disabled
+                  style="width:400px"
+                />
+              </el-form-item>
+              <el-form-item label="电话">
+                <el-input
+                  v-model="formData.companyPhone"
                   disabled
                   style="width:400px"
                 />
               </el-form-item>
               <el-form-item label="邮箱">
                 <el-input
+                  v-model="formData.mailbox"
                   disabled
                   style="width:400px"
                 />
               </el-form-item>
               <el-form-item label="备注">
                 <el-input
+                  v-model="formData.remarks"
                   type="textarea"
                   :rows="3"
                   disabled
@@ -116,32 +132,64 @@
 </template>
 
 <script>
-import { getRoleList } from '@/api/setting'
+import {
+  getRoleList,
+  getCompanyInfo,
+  deleteRole
+} from '@/api/setting'
+import {
+  mapGetters
+} from 'vuex'
 export default {
   data() {
     return {
-      list: [],//承接数组
+      list: [], //承接数组
       page: {
         //放置页码及相关数据
         page: 1,
         pagesize: 10,
-        total: 0//默认总数//记录总数
+        total: 0 //默认总数//记录总数
+      },
+      formData: {
+        // 公司信息
       }
     }
   },
+  computed: {
+    ...mapGetters(['companyId'])
+  },
   created() {
-    this.getRoleList()//获取角色列表
+    this.getRoleList() //获取角色列表
+    this.getCompanyInfo()
   },
   methods: {
     async getRoleList() {
-      const { total, rows } = await getRoleList(this.page)
+      const {
+        total,
+        rows
+      } = await getRoleList(this.page)
       this.page.total = total
       this.list = rows
     },
     changePage(newPage) {
       // newPage是当前点击的页面
-      this.page.page = newPage//将当前页码给当前的最新页码
+      this.page.page = newPage //将当前页码给当前的最新页码
       this.getRoleList()
+    },
+    async getCompanyInfo() {
+      this.formData = await getCompanyInfo(this.companyId)
+    },
+    async deleteRole(id) {
+      // 提示
+      try {
+        await this.$confirm('确认删除该角色吗？')
+        // 只有点击了确定 才能进入下一步
+        await deleteRole(id)//调用删除接口
+        this.getRoleList()//重新加载数据
+        this.$message.success('删除角色成功')
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 }
